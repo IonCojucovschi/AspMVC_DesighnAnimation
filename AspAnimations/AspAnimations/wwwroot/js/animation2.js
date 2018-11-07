a69f94ae46be6243ff53710947e03ce3fe820255
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-11-07 12:12:46 +0200

    first animation asp net core snow

---
+++ AspAnimations/AspAnimations/wwwroot/js/animation2.js
@@ -0,0 +1,14 @@
+﻿function MoveTwoPointHaos(){
+   var x1,y1;
+   var randomY=$("#containerAnm").height();
+   var randomX=$("#containerAnm").width();
+   var salter=true;
+
+   console.log('starting infinite loop');
+
+     x1=Math.floor((Math.random() * randomX) + 1);
+     y1=Math.floor((Math.random() * randomY) + 1);
+     $("#myobject1").animate({marginTop:y1,marginLeft:x1},3000);
+     $("#myobject2").animate({marginTop:x1,marginLeft:y1},3000,MoveTwoPointHaos);
+
+ }
--- AspAnimations/AspAnimations/wwwroot/js/site.js
+++ AspAnimations/AspAnimations/wwwroot/js/site.js
@@ -2,3 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+$(function(){
+   $("#loadAnim1").on("click",function(){
+
+     $("#anim_container").empty();
+     $(`<div id="startAnim1">Start</div>
+        <div id="containerAnm" style="background:#d0f0f8;height:90%;width:90%;position:absolute;">
+         <div id="myobject" style="border-radius: 50%;background:#000000;color:#ffffff;height:10px;width:10px;margin:0px;"><div>
+        </div>`).appendTo("#anim_container");
+        MoveDivHaos();
+   });
+   $("#loadAnim2").on("click",function(){
+
+     $("#anim_container").empty();
+     $(`<div id="startAnim1">Start</div>
+        <div id="containerAnm" style="background:#d0f0f8;height:90%;width:90%;position:absolute;">
+         <div id="myobject1" style="position:absolute;border-radius: 50%;background:#000000;color:#ffffff;height:10px;width:10px;margin:0px;"></div>
+         <div id="myobject2" style="position:absolute;border-radius: 50%;background:#000000;color:#ffffff;height:10px;width:10px;margin:0px;"></div>
+        </div>`).appendTo("#anim_container");
+        MoveTwoPointHaos();
+   });
+
+    $("#loadAnim3").on("click",function(){
+
+     $("#anim_container").empty();
+     $(`<div id="startAnim1">Start</div>
+        <div id="containerAnm" style="background:#d0f0f8;height:90%;width:90%;position:absolute;">
+        <canvas id="canvas" style="height:100%;width:100%;"></canvas> 
+        </div>`).appendTo("#anim_container");
+       init();
+   });
+});
