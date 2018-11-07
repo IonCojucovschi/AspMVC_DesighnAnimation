93dd5442d69f4c0476f22715a772ae5d54a40093
Cojucovschi Ion <Cojucovschi@gmail.com>
2018-11-07 19:11:22 +0200

    add background animation

--- AspAnimations/AspAnimations/wwwroot/js/site.js
+++ AspAnimations/AspAnimations/wwwroot/js/site.js
@@ -25,5 +25,36 @@ $(function(){
 
     $("#loadAnim3").on("click",function(){
       init();
-   });
+    });
 });
+
+
+///// animation for background content
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("#mainContent").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -1 - 25;
+          var newvalueY = height * pageY * -1 - 50;
+          $('#backgroundImage').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
